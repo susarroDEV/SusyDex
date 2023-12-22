ca2e10496393d97955ed2d82fd1458c4072b5a1e
susarroDEV <susarrodev@gmail.com>
2023-12-22 16:32:00 +0100

    Added PokeIndex to Cards

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -16,6 +16,7 @@ export function PokemonCard() {
             {filteredPokemonList.map(pokemon => {
                 return (
                     <div className={`pkc-container pkc-main-type-${pokemon.type1.toUpperCase()}`} key={pokemon.id}>
+                        <strong className='pkc-number'>{`#${pokemon.id}`}</strong>
                         <img className='pkc-sprite' src={pokemon.sprite} alt={`The Sprite of ${pokemon.name}`} />
                         <h1 className='pkc-name'>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h1>
                         <div className ='pkc-types'>
